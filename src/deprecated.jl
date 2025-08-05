8fe1edbd60c8e55c20caf51fe03e294d82eb1ba0
Anthony D. Blaom <anthony.blaom@gmail.com>
2025-08-05 12:40:04 +1200

    deprecate `classes` on any abstract array of UnivariateFinite's

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -6,3 +6,15 @@
 @deprecate classes(v::CategoricalArray) levels(v)
 @deprecate classes(v::SubArray{<:Any, <:Any, <:CategoricalArray}) levels(v)
 @deprecate classes(d::UnivariateFinite) levels(d)
+
+const ERR_EMPTY_UNIVARIATE_FINITE = ArgumentError(
+    "No `UnivariateFinite` object found from which to extract classes. ")
+
+function classes(ds::AbstractArray{<:Union{Missing,UnivariateFinite}})
+    Base.depwarn(
+        "`classes(v::AbstractArray{<:Union{Missing,UnivariateFinite}})` "*
+            "is deprecated. Use `levels(first(v))` instead. ",
+        :classes,
+    )
+    return levels(first(ds))
+end
